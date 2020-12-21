683cb9e32862083457baad833c4aace98493d986
Kira <kira.eva@protonmail.com>
2020-12-22 00:59:35 +0300

    test rapsheet command

--- src/commands/Moderation/rapsheet.ts
+++ src/commands/Moderation/rapsheet.ts
@@ -1,17 +1,22 @@
-import { findUser } from "../../mongodb/api/userApi";
+import { findOrCreate, findUser } from "../../mongodb/api/userApi";
 import { Command } from "../../types/bdl";
 
 export const command: Command = {
-    name: 'Rapsheet',
+    name: 'rapsheet',
     description: 'Retrieves a given users rapsheet',
     aliases: ['rs'],
     permissions: ["admin", 'moderator', 'janitor'],
-    args: true,
+    // args: true,
     usage: '[id | name]',
 
     async execute(message, args) {
         const id = message.author.id
-        const user = await findUser(id)
+        const user = await findOrCreate(id, message.member)
+
+        if (!user) {
+            return message.channel.send('user not found')
+        }
+
         message.channel.send(`User: ${user.username}\nrapsheet size: ${user.rapsheet.length}`)
     }
 }
