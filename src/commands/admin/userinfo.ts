f4503569db6ccc8acd4704f4ff3bb3e67b423504
Kira <kira.eva@protonmail.com>
2020-12-21 19:19:29 +0300

    Created skeleton for command

---
+++ src/commands/admin/userinfo.ts
@@ -0,0 +1,11 @@
+import { Command } from "../../types/bdl";
+
+export const command: Command = {
+    name: 'UserInfo',
+    description: 'Gets user info',
+    aliases: ['uinfo'],
+
+    async execute(message, args) {
+
+    }
+}
