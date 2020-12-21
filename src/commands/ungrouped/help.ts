03e699e866da7bbadfbc3f93a3ec7f4983c5c3f5
Kira <kira.eva@protonmail.com>
2020-12-21 21:33:13 +0300

    Lists all commands

--- src/commands/ungrouped/help.ts
+++ src/commands/ungrouped/help.ts
@@ -1,4 +1,7 @@
 import { Command } from "../../types/bdl";
+import { bot } from '../../app';
+import { Message, MessageEmbed } from 'discord.js';
+import { embedColor, wrap } from '../../util/styleUtil';
 
 export const command: Command = {
     name: 'help',
@@ -6,6 +9,40 @@ export const command: Command = {
     description: 'Lists all commands, and provides usage for specific commands',
 
     async execute(message, args) {
-        message.channel.send('meow')
+        // const query = args.shift()?.toLowerCase();
+        // if (!query) return
+        AllCommands(message);
     }
+}
+
+function AllCommands(message: Message) {
+    const embed = new MessageEmbed()
+        .setColor(embedColor)
+        .setTitle("Commands")
+
+    bot.commands.get("ungrouped").commands.map(cmd => {
+        embed.addField(cmd.name, cmd.description)
+    })
+
+    bot.commands.map(group => {
+        if (group.name !== "ungrouped")
+            embed.addField(group.name, group.description)
+    })
+
+    message.channel.send(embed)
+}
+
+function InsertCommandEmbed(embed: MessageEmbed, command: Command) {
+    embed.setTitle(command.name);
+    embed.setDescription(command.description);
+
+    if (command.usage) {
+        embed.addField('Usage', wrap(command.usage, '`'));
+    }
+
+    if (command.aliases) {
+        const aliasesString = wrap(command.aliases, '`');
+        embed.addField('aliases: ', aliasesString);
+    }
+    return embed;
 }
