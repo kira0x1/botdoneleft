c1f5fa8d68103c0f6d582cb104c00bf96858d6c5
Kira <kira.eva@protonmail.com>
2020-12-21 21:34:54 +0300

    Added more utility methods
    - createFooter
    - quickEmbed

--- src/util/styleUtil.ts
+++ src/util/styleUtil.ts
@@ -1,3 +1,5 @@
+import { Message, MessageEmbed } from "discord.js";
+
 const redColor = 0xcf274e;
 // const blueColor = 0x4e74e6;
 // const oldBlueColor = 0x6788eb;
@@ -16,4 +18,24 @@ export function wrap(content: string[] | string, wrap: string = '`'): string {
         .filter(str => str !== ``)
         .map(str => wrap + str + wrap)
         .join(' ');
+}
+
+export function createFooter(message: Message): MessageEmbed {
+    const author = message.author
+
+    const embed = new MessageEmbed()
+        .setColor(embedColor)
+        .setFooter(author.username, author.avatarURL({ dynamic: true }))
+        .setTimestamp(Date.now());
+
+    return embed;
+}
+
+export function quickEmbed(message: Message, content: string) {
+    const embed = new MessageEmbed().setTitle(content).setColor(embedColor);
+    message.channel.send(embed);
+}
+
+export function createEmptyField(inline?: boolean | false) {
+    return { name: `\u200b`, value: '\u200b', inline: true };
 }
