9814c8d8d90e2cdc4a7fcb6150a621430a310f80
kira0x1 <kira.eva@protonmail.com>
2021-01-15 15:41:54 +0300

    formatting change

--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,56 @@
-import { LoadCommands } from './system/commandLoader'
-import { token } from './config'
-import { BdlClient } from './types/bdl'
-import { findCommand, sendArgsError } from './util/commandUtil'
-import chalk from 'chalk';
-import { connectToDB } from './mongodb/database';
-import { wrap } from './util/styleUtil';
-import { checkPermission } from './util/discordUtil';
+import { LoadCommands } from "./system/commandLoader";
+import { token } from "./config";
+import { BdlClient } from "./types/bdl";
+import { findCommand, sendArgsError } from "./util/commandUtil";
+import chalk from "chalk";
+import { connectToDB } from "./mongodb/database";
+import { wrap } from "./util/styleUtil";
+import { checkPermission } from "./util/discordUtil";
 
 export const bot = new BdlClient({
-    presence: { activity: { name: 'Socialist\'s Trolling', type: 'WATCHING' } }
-})
+  presence: { activity: { name: "Socialist's Trolling", type: "WATCHING" } }
+});
 
-connectToDB()
+connectToDB();
 
-bot.on('ready', async () => {
-    await LoadCommands(bot)
-    console.log(chalk.bgMagenta.bold(`${bot.user.username} is online!`))
-})
+bot.on("ready", async () => {
+  await LoadCommands(bot);
+  bot.guilds.cache.map((g, i) => {
+    console.log(chalk`{bgBlue.bold ${i}} {bgCyan.bold ${g.name}}`);
+  });
+  console.log(chalk.bgMagenta.bold(`${bot.user.username} is online!`));
+});
 
-bot.on('message', async message => {
-    // Make sure the user is not a bot, and the message starts with the prefix assigned
-    // And make sure its not sent in a dm
-    if (message.author.bot || !message.content.startsWith(bot.prefix) || message.channel.type === 'dm')
-        return
+bot.on("message", async message => {
+  // Make sure the user is not a bot, and the message starts with the prefix assigned
+  // And make sure its not sent in a dm
+  if (message.author.bot || !message.content.startsWith(bot.prefix) || message.channel.type === "dm")
+    return;
 
-    // Split up message into an array and remove the prefix
-    let args = message.content.slice(bot.prefix.length).split(/ +/);
+  // Split up message into an array and remove the prefix
+  let args = message.content.slice(bot.prefix.length).split(/ +/);
 
-    // Remove the first element from the args array ( this is the command name )
-    let commandName = args.shift();
+  // Remove the first element from the args array ( this is the command name )
+  let commandName = args.shift();
 
-    let command = findCommand(commandName)
+  let command = findCommand(commandName);
 
-    // If command not found send a message
-    if (!command)
-        return message.author.send(`command ${wrap(commandName || '')} not found`);
+  // If command not found send a message
+  if (!command)
+    return message.author.send(`command ${wrap(commandName || "")} not found`);
 
-    if (!checkPermission(message.member, command)) {
-        message.member.send("You dont have enough permissions to use that command")
-        return;
-    }
+  if (!checkPermission(message.member, command)) {
+    message.member.send("You dont have enough permissions to use that command");
+    return;
+  }
 
-    if (command.args && args.length === 0) return sendArgsError(command, message)
+  if (command.args && args.length === 0) return sendArgsError(command, message);
 
-    try {
-        command.execute(message, args)
-    }
-    catch (error) {
-        console.error(error)
-    }
-})
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-bot.login(token)
+bot.login(token);
