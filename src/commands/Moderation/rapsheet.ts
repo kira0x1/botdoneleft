5f802509bec2b6d5ef15e80aa84dbc5ddea78f5b
kira0x1 <kira.eva@protonmail.com>
2021-02-08 04:32:13 +0300

    add comments

--- src/commands/Moderation/rapsheet.ts
+++ src/commands/Moderation/rapsheet.ts
@@ -8,14 +8,11 @@ export const command: Command = {
     description: 'Retrieves a given users rapsheet',
     aliases: ['rs'],
     permissions: ["admin", 'moderator', 'janitor'],
-    // args: true,
     usage: '[id | name]',
 
     async execute(message, args) {
         const target = await getTarget(args[0], message) || message.member
-        const id = target.id
-
-        const user = await findOrCreate(id, target)
+        const user = await findOrCreate(target)
 
         if (!user) {
             return message.channel.send('user not found')
@@ -26,28 +23,35 @@ export const command: Command = {
             .setDescription(`${target}\n${wrap(target.id)}`)
             .setThumbnail(target.user.avatarURL({ dynamic: true }))
 
+
         const warnings = []
         user.rapsheet.filter(r => r.punishment === 'warn').map(rap => {
             warnings.push(createRapsheetField(rap))
         })
 
+        // Add warnings to embed
         embed.addField(`Warnings: ${warnings.length}`, warnings.join('\n') || 'none')
+
 
         const mutes = []
         user.rapsheet.filter(r => r.punishment === 'mute').map(rap => {
             mutes.push(createRapsheetField(rap))
         })
 
+        // Add mutes to embed
         embed.addField(`Mutes: ${mutes.length}`, mutes.join('\n') || 'none')
+
 
         const gulags = []
         user.rapsheet.filter(r => r.punishment === 'gulag').map(rap => {
             gulags.push(createRapsheetField(rap))
         })
 
+        // Add gulags to embeds
         embed.addField(`Gulags: ${gulags.length}`, gulags.join('\n') || 'none')
-        embed.addField(`\u200b\nJoined`, target.joinedAt.toLocaleDateString())
 
+
+        embed.addField(`\u200b\nJoined`, target.joinedAt.toLocaleDateString())
         message.channel.send(embed)
     }
 }
